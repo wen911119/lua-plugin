ec0b128e127ffefdcb50d8ad2556257d0c386179
wenjun <771299231@qq.com>
2016-07-07 17:52:02 +0800

    按键精灵的mysql数据库插件基本完成

---
+++ DB.lua
@@ -0,0 +1,57 @@
+--writeTable("user","name=文君,age=24")
+--[[function QMPlugin.writeTable(tableName,data)
+	local f=loadfile(__MQM_RUNNER_LOCAL_PATH_GLOBAL_NAME__.."dbPlugin.mysql")
+	local re=f(tableName,data,1)
+	return re
+end
+--updateTable("user","name=文君","age=25")
+function QMPlugin.updateTable(tableName,target,newData)
+	local f=loadfile(__MQM_RUNNER_LOCAL_PATH_GLOBAL_NAME__.."dbPlugin.mysql")
+	local re=f(tableName,target,newData)
+	return re
+end
+--readTable("user","name=文君")
+function QMPlugin.readTable(tableName,target)
+	local f=loadfile(__MQM_RUNNER_LOCAL_PATH_GLOBAL_NAME__.."dbPlugin.mysql")
+	local re=f(tableName,target,0)
+	return re
+end
+--excuteSql("select * from user where name='tom'")
+function QMPlugin.excuteSql(sql)
+	local f=loadfile(__MQM_RUNNER_LOCAL_PATH_GLOBAL_NAME__.."dbPlugin.mysql")
+	local re=f(sql)
+	return re
+end--]]
+
+
+--local t="select * from user where name='文君'"
+--t=string.gsub(t, " ", "&nbsp")
+local t="name='文君',age=24"
+
+t=t..","
+local kv=""
+local keys=""
+local values=""
+local key=""
+local value=""
+local k=0
+local i=0
+while true do
+	i=string.find(t,",",k)
+	if i == nil then break end
+	kv=string.sub(t,k,i-1)
+    print(kv)	
+	_, _, key, value = string.find(kv, "(.+)%s*=%s*(.+)")
+	
+	keys=keys..key..","
+	values=values..value..","
+	
+	k=i+1
+end
+
+print(keys)
+print(values)
+
+
+
+
---
+++ dbPlugin_mysql_source.lua
@@ -0,0 +1,54 @@
+
+--版本20160315下午18点
+
+function QMPlugin.writeFunction()
+
+    function main(x1,x2,x3)
+		local sql=""
+		if x2==nil then
+			--执行sql
+			sql=x1
+		elseif x3==0 then
+		    --读取表
+			x2=string.gsub(x2,","," AND ")
+			local sql="SELECT * FROM "..x1.." WHERE "..x2
+		
+		elseif x3==1 then
+			--写入表
+			x2=x2..","
+			local kv=""
+			local keys=""
+			local values=""
+			local key=""
+			local value=""
+			local k=0
+			local i=0
+			while true do
+				i=string.find(x2,",",k)
+				if i == nil then break end
+				kv=string.sub(t,k,i-1)
+				print(kv)	
+				_, _, key, value = string.find(kv, "(.+)%s*=%s*(.+)")
+				
+				keys=keys..key..","
+				values=values..value..","
+				
+				k=i+1
+			end
+			sql="INSERT INTO "..x1.." ("..keys..") VALUES("..values..")"
+		else 
+			--更新表
+			local targetStr=string.gsub(x2,","," AND ")
+			local newDataStr=x3
+			sql="UPDATE "..x1.." SET "..newDataStr.." WHERE "..targetStr
+			
+		end
+		sql=string.gsub(sql," ","&nbsp")
+		return LuaAuxLib.URL_OperationGet("http://120.25.248.135:3000/dbOpreate/"..sql)
+    end
+ 
+    local s=string.dump(main)
+    local file=io.open(__MQM_RUNNER_LOCAL_PATH_GLOBAL_NAME__.."dbPlugin.mysql","w")
+    file:write(s)
+    file:close()
+end
